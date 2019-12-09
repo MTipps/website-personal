18d4d74f250121c4ec8477d7dcfba9ef91a70f6b
Megan <mtipps101@gmail.com>
2019-12-09 15:28:51 +0200

    Added skeleton code for footer

---
+++ components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <div>
+      <p>Living, learning, & leveling up</p>
+      <p>one day at a time.</p>
+    </div>
+    <div>
+      <p>LinkedIn</p>
+      <p>Github</p>
+      <p>StackOverflow</p>
+      <p>Codepen</p>
+      <p>Email</p>
+    </div>
+    <div>
+      <p>Created by me &#169; twenty nineteen</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+
+</style>
