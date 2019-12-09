acd13cd8037d801d156ddd1cc65d9fc421ba9680
Megan <mtipps101@gmail.com>
2019-12-09 17:24:06 +0200

    Added skeleton code for the about me section

---
+++ components/AboutMe.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pt-6">
+    <p class="font-sans text-3xl">Hi, I'm a <b class="text-pink-600">front-end developer</b>.</p>
+    <p class="font-sans text-3xl">I produce <b class="text-pink-600">high-quality</b> websites and <b class="text-pink-600">exceptional</b> user experience.</p>
+    <p class="font-sans text-3xl">Do you have a project, idea or problem you'd like to discuss?</p>
+    <p class="font-sans text-3xl">Let's have a chat, <a href="mailto:hey@megantipps.co.za" class="text-pink-600">hey@megantipps.co.za</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutMe'
+}
+</script>
+
+<style scoped>
+
+</style>
