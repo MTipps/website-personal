46f7c076ec583f6ea59a86b3ade0d772b3017bb8
Megan <mtipps101@gmail.com>
2019-12-09 08:28:12 +0200

    Code refactoring

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,5 +1,3 @@
-<!-- TODO: Add links to array -->
-<!-- TODO: Add left text to variables -->
 <!-- TODO: See about adding class names to variables-->
 <!-- TODO: Mobile -->
 
@@ -8,17 +6,15 @@
     <div class="navigation__left flex items-center">
       <div class="rounded-full h-16 w-16 bg-gray-400"></div>
       <div class="info-me pl-4">
-        <p class="font-sans text-2xl text-white">Megan Tipps</p>
-        <p class="font-sans text-base text-white">Front-end Web Developer</p>
+        <p class="font-sans text-2xl text-white">{{ myName }}</p>
+        <p class="font-sans text-base text-white">{{ myTitle }}</p>
       </div>
     </div>
     <div class="navigation__right">
       <ul class="flex flex-col sm:flex-row">
-        <li><a class="font-sans text-base text-white hover:text-pink-600 pl-6" href="#">About Me</a></li>
-        <li><a class="font-sans text-base text-white hover:text-pink-600 pl-6" href="#">Skills</a></li>
-        <li><a class="font-sans text-base text-white hover:text-pink-600 pl-6" href="#">Recent Work</a></li>
-        <li><a class="font-sans text-base text-white hover:text-pink-600 pl-6" href="#">Blog</a></li>
-        <li><a class="font-sans text-base text-white hover:text-pink-600 pl-6" href="mailto:hey@megantipps.co.za">hey@megantipps.co.za</a></li>
+        <li v-for="link in navLinks" :key="link.linkName">
+          <a class="font-sans text-base text-white hover:text-pink-600 pl-6" :href="link.linkRef">{{ link.linkName }}</a>
+        </li>
       </ul>
     </div>
   </nav>
@@ -26,7 +22,20 @@
 
 <script>
 export default {
-  name: 'Navigation'
+  name: 'Navigation',
+  data () {
+    return {
+      myName: 'Megan Tipps',
+      myTitle: 'Front-end Web Developer',
+      navLinks: [
+        { linkName: 'About Me', linkRef: '#' },
+        { linkName: 'Skills', linkRef: '#' },
+        { linkName: 'Recent Work', linkRef: '#' },
+        { linkName: 'Blog', linkRef: '#' },
+        { linkName: 'hey@megantipps.co.za', linkRef: 'mailto:hey@megantipps.co.za' }
+      ]
+    }
+  }
 }
 </script>
 
