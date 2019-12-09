52b8814eb228fc91b83ba4f65d529721c25a30bd
Megan <mtipps101@gmail.com>
2019-12-09 14:26:37 +0200

    Fixed eslint warnings

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,19 +1,20 @@
-<!-- TODO: See about adding class names to variables-->
-<!-- TODO: Mobile -->
-
 <template>
   <nav class="navigation flex items-center justify-between flex-wrap p-6 bg-purple-900 shadow-lg">
     <div class="navigation__left flex items-center mr-10 mb-6 lg:mb-0 w-full lg:w-auto">
-      <div class="rounded-full h-16 w-16 bg-gray-400"></div>
+      <div class="rounded-full h-16 w-16 bg-gray-400" />
       <div class="info-me pl-4">
-        <p class="font-sans text-2xl text-white">{{ myName }}</p>
-        <p class="font-sans text-base text-white">{{ myTitle }}</p>
+        <p class="font-sans text-2xl text-white">
+          {{ myName }}
+        </p>
+        <p class="font-sans text-base text-white">
+          {{ myTitle }}
+        </p>
       </div>
     </div>
     <div class="navigation__right">
       <ul class="flex flex-col lg:flex-row">
         <li v-for="link in navLinks" :key="link.linkName">
-          <a class="font-sans text-base text-white hover:text-pink-600 lg:pl-6" :href="link.linkRef">{{ link.linkName }}</a>
+          <a :href="link.linkRef" class="font-sans text-base text-white hover:text-pink-600 lg:pl-6">{{ link.linkName }}</a>
         </li>
       </ul>
     </div>
