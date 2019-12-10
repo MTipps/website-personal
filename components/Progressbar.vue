e98eec10cc7ea029f4185df8c5737977e1cd4f06
Megan <mtipps101@gmail.com>
2019-12-10 11:11:48 +0200

    Added progressbar for skills

---
+++ components/Progressbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="relative bg-gray-800 w-64 h-5">
+      <div class="absolute bg-pink-600 h-5" :style="'width: ' + progressInnerWidth + '%;'"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Progressbar',
+  props: {
+    progressInnerWidth: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
