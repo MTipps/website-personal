67a1880294cbad5f1f0505ebff08e9040413774b
Megan <mtipps101@gmail.com>
2019-12-10 13:56:12 +0200

    Added recent work data to store

---
+++ store/recentWorkData.js
@@ -0,0 +1,25 @@
+export const state = () => ({
+  recentWorks: [
+    {
+      image: 'logo',
+      title: 'company name',
+      description: 'description',
+      techUsed: 'list of tech used',
+      link: 'Link to Site'
+    },
+    {
+      image: 'logo1',
+      title: 'company name1',
+      description: 'description1',
+      techUsed: 'list of tech used1',
+      link: 'Link to Site1'
+    },
+    {
+      image: 'logo2',
+      title: 'company name2',
+      description: 'description2',
+      techUsed: 'list of tech used2',
+      link: 'Link to Site2'
+    }
+  ]
+})
