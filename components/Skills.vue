ceda430f373bffb840c4cb8790aaad2a1f603c74
Megan <mtipps101@gmail.com>
2019-12-10 10:41:33 +0200

    Added skeleton code for skills section

---
+++ components/Skills.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="pt-6 text-center">
+    <p class="font-sans font-bold text-4xl text-pink-600">What can I do?</p>
+    <p class="font-sans text-lg">Learning new languages and frameworks is what keeps me going. Here are a list of my current skills:</p>
+    <div class="pt-6">
+      <div class="flex justify-center pb-6">
+        <p>skill image</p>
+        <p>skill progress</p>
+      </div>
+      <div class="flex justify-center pb-6">
+        <p>skill image</p>
+        <p>skill progress</p>
+      </div>
+      <div class="flex justify-center pb-6">
+        <p>skill image</p>
+        <p>skill progress</p>
+      </div>
+      <div class="flex justify-center pb-6">
+        <p>skill image</p>
+        <p>skill progress</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills'
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="mx-6 lg:mx-16">
     <app-about-me />
+    <app-skills />
   </div>
 </template>
 
 <script>
 import AboutMe from '../components/AboutMe'
+import Skills from '../components/Skills'
 
 export default {
   components: {
-    appAboutMe: AboutMe
+    appAboutMe: AboutMe,
+    appSkills: Skills
   }
 }
 </script>
