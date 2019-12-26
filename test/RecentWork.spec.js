2488547774d4c4157106183544dfe97f1b56fa8f
Megan <mtipps101@gmail.com>
2019-12-26 11:54:55 +0200

    Fixed RecentWork.spec.js

--- test/RecentWork.spec.js
+++ test/RecentWork.spec.js
@@ -1,44 +1,113 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
+import Vue from 'vue'
+import VueLazyload from 'vue-lazyload'
 import RecentWork from '@/components/RecentWork.vue'
 
 let wrapper
 let store
 let state
 const localVue = createLocalVue()
+const recentWorkHeader = ''
+const recentWorkSubHeader = ''
+const recentWorks = [
+  {
+    image: '',
+    title: '',
+    description: '',
+    techUsed: [
+      {
+        techName: '',
+        techIcon: [ '' ]
+      }
+    ],
+    link: ''
+  }
+]
+const vueLazyLoad = Vue.use(VueLazyload, {
+  preLoad: 1,
+  attempt: 1
+})
+
 localVue.use(Vuex)
 
 beforeEach(() => {
   state = {
-    recentWorkData: {
-      recentWorks: [
-        {
-          image: 'https://via.placeholder.com/728x90',
-          title: 'Test Company Name',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas rutrum felis dui, congue mollis massa fermentum eget. Etiam sed suscipit sapien. Integer imperdiet odio eget arcu condimentum, sed viverra',
-          techUsed: [ 'HTML', 'CSS', 'Javascript', 'Typescript' ],
-          link: 'Link to Site'
-        }
-      ]
-    },
-    techStackData: {
-      techStack: [
-        {
-          techName: 'HTML',
-          techIcon: '<svg class="fill-current text-gray-800 hover:text-pink-600 inline-block h-12 w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">\n' +
-            '<path fill="currentColor" d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z"></path>\n' +
-            '</svg>',
-          techProgress: 80
-        }
-      ]
+    techStackLabels: {
+      html: {
+        name: 'html'
+      },
+      css: {
+        name: 'css'
+      },
+      sass: {
+        name: 'sass'
+      },
+      javascript: {
+        name: 'javascript'
+      },
+      jquery: {
+        name: 'jquery'
+      },
+      vuejs: {
+        name: 'vuejs'
+      },
+      nuxtjs: {
+        name: 'nuxtjs'
+      },
+      react: {
+        name: 'react'
+      },
+      angular: {
+        name: 'angular'
+      },
+      typescript: {
+        name: 'typescript'
+      },
+      wordpress: {
+        name: 'wordpress'
+      },
+      contentful: {
+        name: 'contentful'
+      },
+      linkedin: {
+        name: 'linkedin'
+      },
+      github: {
+        name: 'github'
+      },
+      stackoverflow: {
+        name: 'stackoverflow'
+      },
+      codepen: {
+        name: 'codepen'
+      },
+      email: {
+        name: 'email'
+      }
     }
   }
 
-  store = new Vuex.Store({ state })
+  store = new Vuex.Store({
+    modules: {
+      techStack: {
+        namespaced: true,
+        state
+      }
+    }
+  })
 
   wrapper = mount(RecentWork, {
     store,
-    localVue
+    localVue,
+    propsData: {
+      recentWorkHeader,
+      recentWorkSubHeader,
+      recentWorks
+    },
+    directives: {
+      vueLazyLoad
+    }
   })
 })
 
