62dfdeb9ba38aa813419511ca1560cbdc5007dc7
Megan <mtipps101@gmail.com>
2020-02-15 15:06:47 +0200

    Update to environment

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ export default {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
     CTF_CPA_ACCESS_TOKEN: process.env.CTF_CPA_ACCESS_TOKEN,
-    CTF_ENVIRONMENT: process.env.CTF_ENVIRONMENT
+    CTF_ENVIRONMENT: process.env.CTF_ENVIRONMENT,
+    APP_ENVIRONMENT: process.env.APP_ENVIRONMENT
   },
   /*
   ** Customize the progress-bar color
