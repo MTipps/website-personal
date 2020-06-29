1e263e40d5334f714ba3b990c628bec9cd69cb52
Megan <mtipps101@gmail.com>
2020-06-29 19:53:34 +0200

    Updated footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,63 +1,99 @@
 <template>
-  <footer class="bg-purple-900 p-6 mt-6 text-center">
-    <div class="mb-6">
-      <font-awesome-icon :icon="['fab', 'linkedin']" />
-      <p
-        v-for="phrase in catchphrase"
-        :key="phrase.text"
-        class="font-sans font-bold text-xl text-white"
-      >
-        {{ phrase.text }}
-      </p>
+  <footer class="footer">
+    <div class="footer__motto">
+      <p class="motto-text">Living, learning, & leveling up</p>
+      <p class="motto-text">one day at a time.</p>
     </div>
-    <div class="flex justify-center mb-6">
-      <a
-        v-for="social in socialMedia"
-        :key="social.linkRef"
-        :href="social.linkRef"
-        class="pr-6"
-        target="_blank"
-      >
-        <app-icon :icon-name="social.linkIcon" icon-class="fill-current text-white hover:text-pink-600 inline-block h-8 w-8" class="h-8 w-8" />
+
+    <div class="footer__social-media">
+      <a href="https://www.linkedin.com/in/megantipps/">
+        <font-awesome-icon :icon="['fab', 'linkedin']" size="2x" class="social-media-icon" />
+      </a>
+      <a href="https://github.com/MTipps/">
+        <font-awesome-icon :icon="['fab', 'github']" size="2x" class="social-media-icon" />
+      </a>
+      <a href="https://stackoverflow.com/users/6733443/megan">
+        <font-awesome-icon :icon="['fab', 'stack-overflow']" size="2x" class="social-media-icon" />
+      </a>
+      <a href="https://codepen.io/megantipps">
+        <font-awesome-icon :icon="['fab', 'codepen']" size="2x" class="social-media-icon" />
       </a>
     </div>
-    <div>
-      <p class="font-sans text-base text-white">
-<!--        {{ createdByText }} &#169; {{ new Date().getFullYear() }}-->
+
+    <div class="footer__copyright">
+      <p class="copyright-text">
+        Created by me &#169; {{ new Date().getFullYear() }}
       </p>
-      <p
-        v-for="phrase in createdWith"
-        :key="phrase.text"
-        v-html="phrase.text"
-        class="font-sans text-base text-white"
-      />
+      <p class="copyright-text">
+        Website created with
+        <a href="https://nuxtjs.org/" class="copyright-link">NuxtJS</a>
+        and hosted on
+        <a href="https://www.netlify.com/" class="copyright-link">Netlify</a>.
+      </p>
     </div>
   </footer>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import Icon from './Icon'
-
 export default {
-  name: 'Footer',
-  components: {
-    appIcon: Icon
-  },
-  computed: {
-    ...mapState('footer', [
-      'catchphrase',
-      'socialMedia',
-      'createdByText',
-      'createdWith'
-    ])
-  },
-  mounted () {
-    this.$store.dispatch('footer/getFooterItems')
-  }
+  name: 'Footer'
 }
 </script>
 
-<style scoped>
+<!-- TODO: Update with correct mixins -->
+<style lang="scss" scoped>
+.footer {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  row-gap: 30px;
+  background-color: map-get($colours, 'colour-slate-gray');
+  padding: 30px;
 
+  &__motto,
+  &__social-media,
+  &__copyright {
+    justify-self: center;
+    align-self: center;
+  }
+
+  &__motto {
+    .motto-text {
+      font: bold 18px/23px map-get($fonts, 'pt-sans');
+      color: map-get($colours, 'colour-merino');
+      text-align: center;
+    }
+  }
+
+  &__social-media {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 20px;
+
+    .social-media-icon {
+      color: map-get($colours, 'colour-tulip-tree');
+
+      &:hover {
+        color: map-get($colours, 'colour-pelorous');
+      }
+    }
+  }
+
+  &__copyright {
+    .copyright-text {
+      font: normal 12px/13px map-get($fonts, 'pt-sans');
+      color: map-get($colours, 'colour-merino');
+      text-align: center;
+
+      &:first-of-type {
+        margin-bottom: 5px;
+      }
+    }
+
+    .copyright-link {
+      color: map-get($colours, 'colour-pelorous');
+      text-decoration: none;
+    }
+  }
+}
 </style>
