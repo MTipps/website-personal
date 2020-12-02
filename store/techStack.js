70162cd1c0386f1924fb653f8d410eee543cfa23
Megan <mtipps101@gmail.com>
2020-12-02 12:56:35 +0200

    Updates

--- store/techStack.js
+++ store/techStack.js
@@ -1,65 +1,65 @@
 // TODO: Add info for each teck
 export const state = () => ({
   techStackHeading: {
-    heading: 'What can I do?',
+    heading: 'Skills',
     subHeading: 'Learning new languages and frameworks is what keeps me going. Here are a list of my current skills:'
   },
   techStackItems: {
     html: {
       techName: 'HTML',
       techProgress: 80,
-      techInfo: 'Some info text.',
-      techLink: 'http://some.link',
-      fontAwesome: 'fa-html5'
+      techInfo: 'Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser.',
+      techLink: 'https://en.wikipedia.org/wiki/HTML',
+      fontAwesome: 'html5'
     },
     css: {
       techName: 'CSS',
       techProgress: 80,
-      techInfo: 'Some info text.',
-      techLink: 'http://some.link',
-      fontAwesome: 'fa-css3-alt'
+      techInfo: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.',
+      techLink: 'https://en.wikipedia.org/wiki/CSS',
+      fontAwesome: 'css3-alt'
     },
     sass: {
       techName: 'SASS',
       techProgress: 80,
-      techInfo: 'Some info text.',
-      techLink: 'http://some.link',
-      fontAwesome: 'fa-sass'
+      techInfo: 'Sass is the most mature, stable, and powerful professional grade CSS extension language in the world. ',
+      techLink: 'https://sass-lang.com/',
+      fontAwesome: 'sass'
     },
     javascript: {
       techName: 'Javascript',
       techProgress: 80,
-      techInfo: 'Some info text.',
-      techLink: 'http://some.link',
-      fontAwesome: 'fa-js'
+      techInfo: 'JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else. (Okay, not everything, but it is amazing what you can achieve with a few lines of JavaScript code.)',
+      techLink: 'https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript',
+      fontAwesome: 'js'
     },
     vuejs: {
       techName: 'VueJS',
       techProgress: 50,
-      techInfo: 'Some info text.',
-      techLink: 'http://some.link',
-      fontAwesome: 'fa-vuejs'
+      techInfo: 'Vue s a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. On the other hand, Vue is also perfectly capable of powering sophisticated Single-Page Applications when used in combination with modern tooling and supporting libraries.',
+      techLink: 'https://vuejs.org/',
+      fontAwesome: 'vuejs'
     },
     react: {
       techName: 'React',
       techProgress: 30,
-      techInfo: 'Some info text.',
-      techLink: 'http://some.link',
-      fontAwesome: 'fa-react'
+      techInfo: 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
+      techLink: 'https://reactjs.org/',
+      fontAwesome: 'react'
     },
     angular: {
       techName: 'Angular',
       techProgress: 30,
-      techInfo: 'Some info text.',
-      techLink: 'http://some.link',
-      fontAwesome: 'fa-angular'
+      techInfo: 'Angular is a platform and framework for building single-page client applications using HTML and TypeScript. Angular is written in TypeScript. It implements core and optional functionality as a set of TypeScript libraries that you import into your apps.',
+      techLink: 'https://angular.io/',
+      fontAwesome: 'angular'
     },
     wordpress: {
       techName: 'Wordpress',
       techProgress: 70,
-      techInfo: 'Some info text.',
-      techLink: 'http://some.link',
-      fontAwesome: 'fa-wordpress'
+      techInfo: 'WordPress is software designed for everyone, emphasizing accessibility, performance, security, and ease of use. We believe great software should work with minimum set up, so you can focus on sharing your story, product, or services freely. The basic WordPress software is simple and predictable so you can easily get started. It also offers powerful features for growth and success.',
+      techLink: 'https://wordpress.org/',
+      fontAwesome: 'wordpress'
     }
   }
 })
